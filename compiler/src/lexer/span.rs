7c1932a361fa7decc5aac3df3b2928f0c56ab090
Misbah Khursheed <m.misbahkhursheed@gmail.com>
2025-12-23 23:14:44 +0530

    added span

---
+++ compiler/src/lexer/span.rs
@@ -0,0 +1,12 @@
+//gonna use for error and diagnostics
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Span {
+    pub fn new(start: usize, end: usize) -> Self {
+        Self { start, end }
+    }
+}
